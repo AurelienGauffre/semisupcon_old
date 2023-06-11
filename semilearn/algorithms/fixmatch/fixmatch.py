1d0d26643b8540beb27b10c8d977ba4e5a927c80
gauffrea <aurelien.gauffre@gmail.com>
2023-06-12 00:47:47 +0200

    Add NOCAT contrastive and CE detach parameter

--- semilearn/algorithms/fixmatch/fixmatch.py
+++ semilearn/algorithms/fixmatch/fixmatch.py
@@ -198,9 +198,9 @@ class SemiSupCon(AlgorithmBase):
             else:
                 outs_x_lb = self.model(x_lb, contrastive=True,detach_ce=False)
                 logits_x_lb, contrastive_x_lb = outs_x_lb['logits'], outs_x_lb['contrastive_feats']
-                outs_x_ulb_s_0 = self.model(x_ulb_s_0, contrastive=True,detach_ce=False)
+                outs_x_ulb_s_0 = self.model(x_ulb_s_0, contrastive=True,detach_ce=True)
                 logits_x_ulb_s_0, contrastive_x_ulb_s_0 = outs_x_ulb_s_0['logits'], outs_x_ulb_s_0['contrastive_feats']
-                outs_x_ulb_s_1 = self.model(x_ulb_s_1, contrastive=True,detach_ce=False)
+                outs_x_ulb_s_1 = self.model(x_ulb_s_1, contrastive=True,detach_ce=True)
                 logits_x_ulb_s_1, contrastive_x_ulb_s_1 = outs_x_ulb_s_1['logits'], outs_x_ulb_s_1['contrastive_feats']
 
                 with torch.no_grad():
