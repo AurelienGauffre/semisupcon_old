08dca71b67df8508b7c03728488ec7c7d0759919
gauffrea <aurelien.gauffre@gmail.com>
2023-08-24 17:28:59 +0200

    fix

--- semilearn/algorithms/fixmatch/fixmatch.py
+++ semilearn/algorithms/fixmatch/fixmatch.py
@@ -469,72 +469,72 @@ class SemiSupConProto(AlgorithmBase):
 
             return out_dict, log_dict
 
-        def evaluate(self, eval_dest='eval', out_key='contrastive_feats', return_logits=False):
-            """
-            evaluation function overided from base class algorithmbase (the function is called in core/hook/evaluation)
-            """
-            self.model.eval()
-            self.ema.apply_shadow()
+    def evaluate(self, eval_dest='eval', out_key='contrastive_feats', return_logits=False):
+        """
+        evaluation function overided from base class algorithmbase (the function is called in core/hook/evaluation)
+        """
+        self.model.eval()
+        self.ema.apply_shadow()
 
-            eval_loader = self.loader_dict[eval_dest]
-            total_loss = 0.0
-            total_num = 0.0
-            y_true = []
-            y_pred = []
-            # y_probs = []
-            y_logits = []
-            with torch.no_grad():
-                for data in eval_loader:
-                    x = data['x_lb']
-                    y = data['y_lb']
+        eval_loader = self.loader_dict[eval_dest]
+        total_loss = 0.0
+        total_num = 0.0
+        y_true = []
+        y_pred = []
+        # y_probs = []
+        y_logits = []
+        with torch.no_grad():
+            for data in eval_loader:
+                x = data['x_lb']
+                y = data['y_lb']
 
-                    if isinstance(x, dict):
-                        x = {k: v.cuda(self.gpu) for k, v in x.items()}
-                    else:
-                        x = x.cuda(self.gpu)
-                    y = y.cuda(self.gpu)
+                if isinstance(x, dict):
+                    x = {k: v.cuda(self.gpu) for k, v in x.items()}
+                else:
+                    x = x.cuda(self.gpu)
+                y = y.cuda(self.gpu)
 
-                    num_batch = y.shape[0]
-                    total_num += num_batch
+                num_batch = y.shape[0]
+                total_num += num_batch
 
-                    # similarity_to_proto = contrastive_x_ulb_w @ proto_proj.t()  # (N, K) = (N, D) @ (D, K) equivalent des softmax
-                    # print(f"similarity to proto first line : {similarity_to_proto[0, :]} and first label {y_ulb[0]}")
-                    #
-                    # pseudo_label = torch.argmax(similarity_to_proto, dim=1)
-                    # maskbool = torch.max(similarity_to_proto, dim=1)[0] > self.p_cutoff
-                    # mask_sum = maskbool.sum()  # number of samples with high confidence
-                    #
-                    # contrastive_x_all = torch.cat((contrastive_x_lb, contrastive_x_ulb_s_0[maskbool],
-                    #                                contrastive_x_ulb_s_1[maskbool], proto_proj), dim=0)
+                # similarity_to_proto = contrastive_x_ulb_w @ proto_proj.t()  # (N, K) = (N, D) @ (D, K) equivalent des softmax
+                # print(f"similarity to proto first line : {similarity_to_proto[0, :]} and first label {y_ulb[0]}")
+                #
+                # pseudo_label = torch.argmax(similarity_to_proto, dim=1)
+                # maskbool = torch.max(similarity_to_proto, dim=1)[0] > self.p_cutoff
+                # mask_sum = maskbool.sum()  # number of samples with high confidence
+                #
+                # contrastive_x_all = torch.cat((contrastive_x_lb, contrastive_x_ulb_s_0[maskbool],
+                #                                contrastive_x_ulb_s_1[maskbool], proto_proj), dim=0)
 
-                    out = self.model(x)
-                    contrastive_feats = out[out_key]
-                    proto_proj = out['proto_proj']
-                    similarity_to_proto = contrastive_feats @ proto_proj.t()  # (N, K) = (N, D) @ (D, K) equivalent des softmax
-                    pred = torch.argmax(similarity_to_proto, dim=1)
+                out = self.model(x)
+                contrastive_feats = out[out_key]
+                proto_proj = out['proto_proj']
+                similarity_to_proto = contrastive_feats @ proto_proj.t()  # (N, K) = (N, D) @ (D, K) equivalent des softmax
+                pred = torch.argmax(similarity_to_proto, dim=1)
 
-                    # loss = F.cross_entropy(logits, y, reduction='mean', ignore_index=-1)
-                    y_true.extend(y.cpu().tolist())
-                    y_pred.extend(pred.cpu().tolist())
-                    # y_logits.append(logits.cpu().numpy())
-                    # total_loss += loss.item() * num_batch
-            y_true = np.array(y_true)
-            y_pred = np.array(y_pred)
-            # y_logits = np.concatenate(y_logits)
-            top1 = accuracy_score(y_true, y_pred)
-            balanced_top1 = balanced_accuracy_score(y_true, y_pred)
-            precision = precision_score(y_true, y_pred, average='macro')
-            recall = recall_score(y_true, y_pred, average='macro')
-            F1 = f1_score(y_true, y_pred, average='macro')
+                # loss = F.cross_entropy(logits, y, reduction='mean', ignore_index=-1)
+                y_true.extend(y.cpu().tolist())
+                y_pred.extend(pred.cpu().tolist())
+                # y_logits.append(logits.cpu().numpy())
+                # total_loss += loss.item() * num_batch
+        y_true = np.array(y_true)
+        y_pred = np.array(y_pred)
+        # y_logits = np.concatenate(y_logits)
+        top1 = accuracy_score(y_true, y_pred)
+        balanced_top1 = balanced_accuracy_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred, average='macro')
+        recall = recall_score(y_true, y_pred, average='macro')
+        F1 = f1_score(y_true, y_pred, average='macro')
 
-            cf_mat = confusion_matrix(y_true, y_pred, normalize='true')
-            self.print_fn('confusion matrix:\n' + np.array_str(cf_mat))
-            self.ema.restore()
-            self.model.train()
+        cf_mat = confusion_matrix(y_true, y_pred, normalize='true')
+        self.print_fn('confusion matrix:\n' + np.array_str(cf_mat))
+        self.ema.restore()
+        self.model.train()
 
-            eval_dict = { eval_dest + '/top-1-acc': top1, # eval_dest + '/loss': total_loss / total_num,
-                         eval_dest + '/balanced_acc': balanced_top1, eval_dest + '/precision': precision,
-                         eval_dest + '/recall': recall, eval_dest + '/F1': F1}
-            # if return_logits:
-            #     eval_dict[eval_dest + '/logits'] = y_logits
-            return eval_dict
+        eval_dict = { eval_dest + '/top-1-acc': top1, # eval_dest + '/loss': total_loss / total_num,
+                     eval_dest + '/balanced_acc': balanced_top1, eval_dest + '/precision': precision,
+                     eval_dest + '/recall': recall, eval_dest + '/F1': F1}
+        # if return_logits:
+        #     eval_dict[eval_dest + '/logits'] = y_logits
+        return eval_dict
