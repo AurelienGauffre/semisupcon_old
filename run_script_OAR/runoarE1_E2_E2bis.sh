de9af917d748f089b4afb39f00081b89f5eb66d0
gauffrea <aurelien.gauffre@gmail.com>
2024-03-05 03:44:35 +0100

    Add yaml save

--- run_script_OAR/runoarE1_E2_E2bis.sh
+++ run_script_OAR/runoarE1_E2_E2bis.sh
@@ -2,6 +2,8 @@ cd ~/semisupcon
 . envsemisupcon/bin/activate
 git pull
 nohup python3 train.py --c ./config/configE1.yaml &
+sleep 3
 nohup python3 train.py --c ./config/configE2.yaml &
+sleep 3
 nohup python3 train.py --c ./config/configE2bis.yaml &
 wait
