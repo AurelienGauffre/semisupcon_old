c4bd2856d8a2f3b13fcbef88358a3a310f42faae
gauffrea <aurelien.gauffre@gmail.com>
2024-03-05 15:38:44 +0100

    Add yaml save

--- jean_run_A100.sh
+++ jean_run_A100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # usage :
-# . jean_run.sh E1 50 to tun the job E1 with 50 hours of walltime
-# . jean_run.sh E1_E2 50 to tun the job E1 and E2 on same GPU with 50 hours of walltime
+# . jean_run.sh E1 50 to run the job E1 with 50 hours of walltime
+# . jean_run.sh E1_E2 50 to run the job E1 and E2 on same GPU with 50 hours of walltime
 
 # Check if at least one argument is provided
 if [ "$#" -lt 1 ]; then
@@ -17,7 +17,7 @@ JOB_NAME_STRING="${JOB_NAMES[*]}"
 JOB_NAME_FILE="${JOB_NAME_STRING// /_}"
 
 # Default walltime value
-WALLTIME="99"
+WALLTIME="20"
 
 # If a second argument is provided, it is assumed to be the walltime
 if [ ! -z "$2" ]; then
@@ -27,15 +27,15 @@ fi
 # Create the directory for the script if it does not exist
 mkdir -p ./run_script
 
-# Create the SLURM script with a concatenated job name
+# Start creating the SLURM script with a concatenated job name
 cat <<EOF > "./run_script/auto_script${JOB_NAME_FILE}.slurm"
 #!/bin/bash
-#SBATCH --account=cgs@v100
+#SBATCH --account=cgs@a100
 #SBATCH --job-name=${JOB_NAME_FILE}
 ##SBATCH -C v100-16g                 # uncomment to target only 16GB V100 GPU
 ##SBATCH -C v100-32g                 # uncomment to target only 32GB V100 GPU
-#SBATCH --partition=gpu_p2          # uncomment for gpu_p2 partition (32GB V100 GPU)
-
+##SBATCH --partition=gpu_p2          # uncomment for gpu_p2 partition (32GB V100 GPU)
+#SBATCH -C a100                     # uncomment for gpu_p5 partition (80GB A100 GPU)
 #SBATCH --nodes=1                    # we request one node
 #SBATCH --ntasks-per-node=1          # with one task per node (= number of GPUs here)
 #SBATCH --gres=gpu:1                 # number of GPUs per node (max 8 with gpu_p2, gpu_p4, gpu_p5)
@@ -43,9 +43,14 @@ cat <<EOF > "./run_script/auto_script${JOB_NAME_FILE}.slurm"
 #SBATCH --cpus-per-task=10           # number of cores per task (1/4 of the 4-GPUs node)
 
 #SBATCH --hint=nomultithread         # hyperthreading is deactivated
-#SBATCH --qos=qos_gpu-t4 #for jobs > 100h
+
+EOF
+
+
+
+cat <<EOF >> "./run_script/auto_script${JOB_NAME_FILE}.slurm"
 #SBATCH --time=${WALLTIME}:00:00     # maximum execution time requested (HH:MM:SS)
-#SBATCH --output=OAR.%j.out    # name of output file
+#SBATCH --output=OAR.%j.${JOB_NAME_FILE}.out    # name of output file
 
 module purge
 module load pytorch-gpu/py3/1.13.0
@@ -67,3 +72,6 @@ git pull
 
 echo "SLURM script created: ./run_script/auto_script${JOB_NAME_FILE}.slurm"
 sbatch "./run_script/auto_script${JOB_NAME_FILE}.slurm"
+
+
+
