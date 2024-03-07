71b8aaf6b3b3fa71eda338a37a33b4083689be52
gauffrea <aurelien.gauffre@gmail.com>
2024-03-07 10:43:22 +0100

    Add yaml save

--- config/_config_gen.py
+++ config/_config_gen.py
@@ -9,9 +9,9 @@ def copy_config_files(current_dir):
     # Loop through each file in the directory
     for file in files:
         # Check if the file starts with "configD" and is a YAML file
-        if file.startswith("configD") and file.endswith(".yaml"):
+        if file.startswith("configE") and file.endswith(".yaml"):
             # Construct the new file name by replacing "configD" with "configDflex"
-            new_file = file.replace("configD", "configDflex")
+            new_file = file.replace("configE", "configEcifar100-400-")
 
             # Copy the file to the new file with updated name
             shutil.copyfile(os.path.join(current_dir, file), os.path.join(current_dir, new_file))
