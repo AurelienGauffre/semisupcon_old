72f155f5193ba88d5cef2ed590d198a2510c7cc4
gauffrea <aurelien.gauffre@gmail.com>
2024-03-20 17:22:06 +0100

    config

--- config/_config_gen.py
+++ config/_config_gen.py
@@ -9,13 +9,14 @@ def copy_config_files(current_dir):
     # Loop through each file in the directory
     for file in files:
         # Check if the file starts with "configD" and is a YAML file
-        if file.startswith("configD") and file.endswith(".yaml"):
-            if "Co" not in file and "flex" not in file:
-            # Construct the new file name by replacing "configD" with "configDflex"
-            #new_file = file.replace("configD", "configEcifar100-400-")
-                new_file = file[:-5] + '-s1' + ".yaml"
-                # Copy the file to the new file with updated name
-                shutil.copyfile(os.path.join(current_dir, file), os.path.join(current_dir, new_file))
+        if file.startswith("configEcifar100-400") and file.endswith(".yaml"):
+
+        # Construct the new file name by replacing "configD" with "configDflex"
+        #new_file = file.replace("configD", "configEcifar100-400-")
+
+            new_file = file[:-19] + 'stl-25' + file[-7:]
+            # Copy the file to the new file with updated name
+            shutil.copyfile(os.path.join(current_dir, file), os.path.join(current_dir, new_file))
 
 # Call the function with the current directory
 print(os.getcwd())
