56770dd08d75dfcb7ec8e57d36b33e558fcffa3b
gauffrea <aurelien.gauffre@gmail.com>
2024-03-20 18:42:46 +0100

    config

--- config/_config_gen.py
+++ config/_config_gen.py
@@ -9,12 +9,13 @@ def copy_config_files(current_dir):
     # Loop through each file in the directory
     for file in files:
         # Check if the file starts with "configD" and is a YAML file
-        if file.startswith("configEcifar100-400") and file.endswith(".yaml"):
+        if file.startswith("configEstl") and file.endswith(".yaml"):
 
         # Construct the new file name by replacing "configD" with "configDflex"
         #new_file = file.replace("configD", "configEcifar100-400-")
 
-            new_file = file[:-19] + 'stl-25' + file[-7:]
+            #new_file = file[:-19] + 'stl-25' + file[-5:]
+            new_file = file[:-5] + 'BF' + '.yaml'
             # Copy the file to the new file with updated name
             shutil.copyfile(os.path.join(current_dir, file), os.path.join(current_dir, new_file))
 
